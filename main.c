ca08fe035768a5730bf1dd8d0eff88fef0e088a8
EvanieWares <evaniewares.com>
2023-08-23 08:42:22 +0200

    fix memory leak

--- main.c
+++ main.c
@@ -11,7 +11,8 @@
 int main(int argc, array argv, array envp)
 {
 	info_t info;
-	string buffer;
+	string buf;
+	char buffer[100];
 	int i = 1;
 	bool pipped = false;
 	string args[2];
@@ -31,8 +32,9 @@ int main(int argc, array argv, array envp)
 		{
 			_write(PROMPT);
 		}
-		buffer = get_buf();
-
+		buf = get_buf();
+		_strcpynn(buffer, buf, 0, 0);
+		free(buf);
 		pid = fork();
 		if (pid == 0)
 		{
@@ -44,7 +46,6 @@ int main(int argc, array argv, array envp)
 		}
 		wait(NULL);
 		/*exec_cmd(buffer, info);*/
-		free(buffer);
 		i++;
 	}
 	return (0);
